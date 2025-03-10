3aa8aaa9df00e913b4e0d4ee84994727272d223d
Owk <kkoctuk2005@gmail.com>
2025-03-10 16:32:08 +0200

    task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,23 @@
 'use strict';
 
 document.addEventListener('click', (e) => {
-  // write code here
+  const wall = e.target.closest('.wall');
+  const spider = document.querySelector('.spider');
+
+  const wallRect = wall.getBoundingClientRect();
+
+  const wallBorder = parseInt(getComputedStyle(wall).borderWidth, 10);
+  const spiderSize = spider.offsetHeight;
+
+  if (wall) {
+    spider.style.left =
+      e.clientX - wallRect.left - wallBorder - spiderSize / 2 + 'px';
+
+    spider.style.top =
+      e.clientY - wallRect.top - wallBorder - spiderSize / 2 + 'px';
+  }
+
+  if (spider.style.top < spiderSize) {
+    spider.style.top = 0;
+  }
 });
